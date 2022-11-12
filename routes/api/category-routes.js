91efbe607b7b1d345c0fc0d6f7a0f96b6677c48c
allenmurfee <allenmurfee123@gmail.com>
2022-11-12 11:36:06 -0500

    category routes written

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,16 +29,40 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const create = await Category.create(req.body);
+    res.status(200).json(create);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const update = await Category.update({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(update);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const destroy = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(destroy);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
