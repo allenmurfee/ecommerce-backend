27ac3df81c5357b670df002c577dcb77302d26d9
allenmurfee <allenmurfee123@gmail.com>
2022-11-12 18:09:26 -0500

    delete request

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,8 +34,18 @@ router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const del = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(del);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
